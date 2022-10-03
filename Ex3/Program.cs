f135b282fb34711c436e16c5f6c0cddb428be632
Saglara <nohaevass@yandex.ru>
2022-10-03 22:59:52 +0300

    Вычисление чисел Фиббоначи

---
+++ Ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вычисление чисел Фиббоначи
+//f(1)=1
+//f(2)=1
+//f(n)=f(n-1)+f(n-2)
+Console.Clear();  
+Console.WriteLine("Введите n");
+double n = Convert.ToDouble(Console.ReadLine());
+
+double Fibbonachi (int n)
+{
+    if(n==1||n==2) return 1;
+    else return Fibbonachi(n-1)+Fibbonachi(n-2);
+}
+for (int i=1; i<10;i++)
+{
+    Console.WriteLine(Fibbonachi(i));
+}
