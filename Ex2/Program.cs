249a8d86ad7a434479ca831da33d60326fbc6840
Saglara <nohaevass@yandex.ru>
2022-10-03 22:53:37 +0300

    Вычисление факториала через рекурсию

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычисление факториала через рекурсию
+Console.Clear();  
+Console.WriteLine("Введите n");
+double n = Convert.ToDouble(Console.ReadLine());
+
+double Factorial (int n)
+{
+    if(n==1) return 1;
+    else return n*Factorial(n-1);
+}
+for (int i=1; i<=n;i++)
+{
+    Console.WriteLine($"{i}!={Factorial (i)}");
+}
